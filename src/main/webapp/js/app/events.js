bec9e097201e78db8b93684f4d8169ea796bde34
stronquens <stronquens@gmail.com>
2016-02-07 18:28:38 +0100

    Rediseño pagina de login estilo material design final

--- src/main/webapp/js/app/events.js
+++ src/main/webapp/js/app/events.js
@@ -1,9 +1,16 @@
 
 function pageEvents() {
 
-    $(document).ready(function () { 
-       oController.init();
+    oController.init();
+
+    $('.logout').click(function() {
+        //Cookies().eliminarCookie('amgTwitterToken');
+        //Cookies().eliminarCookie('amgTwitterSecret');
+        Cookies().crearCookie("amgTwitterToken", null);
+        Cookies().crearCookie("amgTwitterSecret", null);
+        //window.location = "../index.html";
+        return false;
     });
-    
+
 }
 
--- src/main/webapp/js/pages/Login.js
+++ src/main/webapp/js/pages/Login.js
@@ -2,24 +2,46 @@ $(document).ready(function() {
     /*
      * Requiere el uso de Cookies.js -- important --
      */
-
-// Obtenemos las cookies y si existen las mostramos y sino las creamos
+    $("h1").show(2000);
+    function animation() {
+        if ($("h1").is(":visible")) {
+            $("h1").hide("slow", function() {
+                $('.spinner').show("slow");
+            });
+        } else {
+            $(".spinner").hide("slow", function() {
+                $('h1').show("slow");
+            });
+        }
+    }
+    ;
+    // Obtenemos las cookies y si existen las mostramos y sino las creamos
     var cookie1 = Cookies().leerCookie("amgTwitterToken");
     var cookie2 = Cookies().leerCookie("amgTwitterSecret");
-    if (cookie1 == null && cookie2 == null) {
+    if (cookie1 === null && cookie2 === null) {
         // Crea el boton de iniciar sesion con la url correspondiente
-        (function getButton() {
-            $.ajax({
-                method: "GET",
-                dataType: "json",
-                url: 'http://127.0.0.1:39963/AmgTwitter/login?op=url'
-            }).done(function(data) {
-                var button = "<a href='" + data.url + "' target='popup' class='a-login'";
-                button += "onClick=\"pop_up = window.open(this.href, this.target, 'width=350,height=420'); return false;\">Log in</a>";
-                // button += "<img class='boton' src='./img/login/boton.png'></img></a>";
-                $("#accion").html(button);
-            });
-        })();
+        $.ajax({
+            method: "GET",
+            dataType: "json",
+            async: false,
+            url: 'http://127.0.0.1:39963/AmgTwitter/login?op=url'
+        }).done(function(data) {
+            var button = "<a href='" + data.url + "' target='popup' class='a-login'";
+            button += "onClick=\"pop_up = window.open(this.href, this.target, 'width=350,height=420'); return false;\">Log in</a>";
+            $("#accion").html(button);
+        });
+        // Regula las animaciones de welcome y spinner
+        $('.a-login').click(function() {
+            if ($("h1").is(":visible")) {
+                animation();
+                var pollTimer = window.setInterval(function() {
+                    if (pop_up.closed !== false) { // !== is required for compatibility with Opera
+                        window.clearInterval(pollTimer);
+                        animation();
+                    }
+                }, 200);
+            }
+        });
         // Comprueba cada segundo si el pop up ha devuelto los parametros y lo cierra
         function checkWindow() {
             try {
